8ff699e2dc542814686d765a398d70cd99d2d399
Lemon <lemon@hirelemon.com>
2016-03-30 16:08:07 -0500

    First commit
    I'll explain what all this does later.

---
+++ js/rounds.js
@@ -0,0 +1,362 @@
+var rounds = [
+  {
+    "title": "Finish This Quote",
+    "category": "movies",
+    "tags": "quotes, movies",
+    "questions":  [
+      {
+        "Q": "DEREK ZOOLANDER: I'm pretty sure there's a lot more to life than being [5 words]",
+        "A": "being really, really, ridiculously good looking"
+      },
+      {
+        "Q": "PRESIDENT MERKIN MUFFLEY: Gentlemen, you can't fight in here! This is [3 words]",
+        "A": "the War Room."
+      },
+      {
+        "Q": "VERBAL KINT: The greatest trick the devil ever pulled was [7 words]",
+        "A": "convincing the world he did not exist."
+      },
+      {
+        "Q": "JESSICA RABBIT: I'm not bad. I'm just [3 words]",
+        "A": "drawn that way."
+      },
+      {
+        "Q": "FERRIS BUELLER: Life moves pretty fast. If you don't stop and take a look around once in a while, [4 words]",
+        "A": "drawn that way."
+      },
+      {
+        "Q": "GEORGE TAYLOR: Take your stinking paws off me, you [4 words]",
+        "A": "damn dirty ape."
+      },
+      {
+        "Q": "EMMETT BROWN: Roads? Where we're going [4 words]",
+        "A": "we don't need roads."
+      },
+      {
+        "Q": "TONY MONTANA: Me, I always tell the truth. [4 words]",
+        "A": "Even when I lie."
+      }
+    ]
+  },
+  {
+    "title": "A General Booze Round",
+    "category": "booze",
+    "tags": "booze, cocktails, movies",
+    "questions": [
+      {
+        "Q": "They do have wine tasting events in Utah. However, when taking part in a wine tasting in Utah, you are not allowed to do this.",
+        "A": "swallow"
+      },
+      {
+        "Q": "Adding this garnish to a glass turns a martini into a gibson.",
+        "A": "onion"
+      },
+      {
+        "Q": "A mixture of whiskey, sweet vermouth, and bitters is called a Manhattan. It was invented by the mother of this famous British Prime Minister.",
+        "A": "Winston Churchill"
+      },
+      {
+        "Q": "In a bill written by Jim Bunning, this type of liquor has been declared by Congress as “America's Native Spirit”",
+        "A": "bourbon"
+      },
+      {
+        "Q": "In The Big Lebowski, The Dude's drink of choice is a “caucasian”, which is his term for what beverage?",
+        "A": "white russian"
+      },
+      {
+        "Q": "Though the list of Oscar winners who are alcoholics is a long one, Jack Lemmon won an Oscar in 1962 for playing an alcoholic in this movie.",
+        "A": "The Days of Wine and Roses"
+      },
+      {
+        "Q": "Ray Milland also won an Oscar for playing an alcoholic, in this 1945 film that has been constantly parodied by The Simpsons.",
+        "A": "The Lost Weekend"
+      },
+      {
+        "Q": "Before Mickey Rourke got a shitload of plastic surgery to get even more weird looking, he was in this Bukowski written film about the life of an alcoholic.",
+        "A": "Barfly"
+      },
+      {
+        "Q": "This comedian (who died at the age of 100) often said “Actually it only takes one drink to get me loaded. Trouble is, I can't remember if it's the thirteenth or the fourteenth”",
+        "A": "George Burns"
+      },
+      {
+        "Q": "This famous American whiskey is actually distilled in a dry county. So while it is legal for them to make it, it would be illegal for them to serve it there.",
+        "A": "Jack Daniel's"
+      }
+    ]
+  },
+  {
+    "title": "What I've Learned From Heist Movies",
+    "category": "movies",
+    "tags": "heist, movies",
+    "questions": [
+      {
+        "Q": "In most heist movies, the bank robbers are outnumbered by the police but they still win. But if the bank robbers are German and the police officer thinks he’s a blues singer, you actually only need the one cop to stop the robbery.",
+        "A": "Die Hard"
+      },
+      {
+        "Q": "It’s pretty common to hire humans as bank guards, but if someone wants to steal a baseball diamond, you might to try hiring non-human help. Also I just remembered that Charles Grodin had a career before those Beethoven movies.",
+        "A": "The Great Muppet Caper"
+      },
+      {
+        "Q": "So it turns out those Mini Coopers can go a lot faster with a trunk full of gold than you’d really expect. I guess we’ll remember this information in another 30 years when they replace Michael Caine with Marky Mark.",
+        "A": "The Italian Job"
+      },
+      {
+        "Q": "Robbing banks might be hard, but robbing casinos is surprisingly easy. It doesn’t matter if it’s 1960 or 2001. It doesn’t matter if you have one eye or a really fake English accent. The only thing you’ll need is a lot of smugness.",
+        "A": "Oceans Eleven"
+      },
+      {
+        "Q": "Bank robbers wear a lot of different masks, but if a heist crew is all black, it turns out white greasepaint is a good enough disguise. Actually, it doesn’t work out that well. I guess the real moral is here is don’t rob a bank if you’re black.",
+        "A": "Dead Presidents"
+      },
+      {
+        "Q": "I’ve learned that if you want to make a heist movie franchise that focuses primarily around people driving fast, you’re going to need a mumbly ex-bouncer guy. Oh sure, you can try to replace him with Lil Bow Wow or Ludacris, but you’ll eventually re-cast the mumbly ex-bouncer.",
+        "A": "The Fast And The Furious"
+      },
+      {
+        "Q": "Superman 3 made $60 million at the box office, but this movie based on the plot of Superman 3 only made $12 million, despite the fact that everyone I know has only seen the latter. Anyway, I guess I really learned that everyone likes the Geto Boys and hates Michael Bolton.",
+        "A": "Office Space"
+      },
+      {
+        "Q": "I was just looking online, and it turns out surfboards cost like $500. That’s more than I thought they’d be, so I guess that’s why surfers decide to rob banks. Also, the idea of Keanu Reeves as a serious professional is like the least believable thing ever.",
+        "A": "Point Break"
+      },
+      {
+        "Q": "I learned that the 3 most common passwords are money, god and sex. I will later learn this is total bullshit. But then, pretty much everything about this movie is bullshit, from the computer terminology to Angelina Jolie’s haircut. I still like it though.",
+        "A": "Hackers"
+      },
+      {
+        "Q": "I’ve learned that you really don’t need to spend money on scripts and promotion. If your movie has Halle Berry’s tits in it, it will make $147 million, despite making absolutely no sense. I’ve also learned how quickly people got tired of that Matrix camera shit.",
+        "A": "Jack Daniel's"
+      }
+    ]
+  },
+  {
+    "title": "2010 In Celebrity Arrests",
+    "category": "celebrities",
+    "tags": "celebrities, arrests, crime, pop culture",
+    "questions": [
+      {
+        "Q": "In August, this less famous half of a very famous hip hop duo was arrested in Miami and charged with illegal possession of MDMA and unprescribed Viagra. Party people in the club insist it would help them cut a rug. They’d throw a deuce in the sky for a shutterbug.",
+        "A": "Big Boi"
+      },
+      {
+        "Q": "In July, this Los Angeles citizen was arrested for driving under the influence. The thing about this guy is, he’s frequently arrested in Los Angeles for driving under the influence. I remember when that happened in 1991, a lot of people in South Central got free VCRs.",
+        "A": "Rodney King"
+      },
+      {
+        "Q": "So it turns out those Mini Coopers can go a lot faster with a trunk full of gold than you’d really expect. I guess we’ll remember this information in another 30 years when they replace Michael Caine with Marky Mark.",
+        "A": "Rodney King"
+      },
+      {
+        "Q": "Politicians never get arrested by cops. They get indicted at the end of a two year investigation, in this case, for making a whole lot of political contributions to cover up certain penis-related improprieties. But at least we get terrific mugshots.",
+        "A": "John Edwards"
+      },
+      {
+        "Q": "In June, this rapper was arrested in Miami (appropriately enough) for driving under the influence on a suspended license. He avoided jail, made $2,000 bail, then shorty got low, low, low, low, low, low, low, low.",
+        "A": "Flo Rida"
+      },
+      {
+        "Q": "In May, this singer/songwriter (of only one song you’ve ever heard) was arrested in Malubu for driving under the influence by the same cop who arrested Mel Gibson. Where can I find a policewoman like that? I guess I’ll stick with Jessie’s policewoman.",
+        "A": "Rick Springfield"
+      },
+      {
+        "Q": "In May, this hype man was arrested in Las Vegas for a handful of warrants, none of which involve drugs. This arrest then is the least humiliating thing he’s done in the last twenty years (the most humiliating was hosting Gathering of the Juggalos this year)",
+        "A": "Flavor Flav"
+      },
+      {
+        "Q": "In April, this prolific actor was arrested in New Orleans on charges of domestic abuse of his third wife (the mother of Kal-El). His $11,000 bond was posted by Dog The Bounty Hunter, who also knows a thing or two about public scorn.",
+        "A": "Nick Cage"
+      },
+      {
+        "Q": "In May, this glam metal singer was arrested in Los Angeles for disorderly conduct and battery of his ex-girlfriend. Like his drummer, he also recorded a sex tape with a Baywatch actress, unfortunately it was one nobody’s heard of.",
+        "A": "Vince Neil"
+      },
+      {
+        "Q": "In October this terrible, terrible rapper woke up outta bed, turned his swag on, and got arrested in Georgia on drug and gun charges. This was the same day of the release of a stupid documentary about his stupid life. Whoever saw it is fucking stupid.",
+        "A": "Soulja Boy"
+      },
+      {
+        "Q": "David Rainey, better known by this sports-equipment nickname, was arrested in June for felony corporal injury, which is like assault to the xtreem. This is the first time anyone’s heard from this former bike messenger since they kicked him off The Real World.",
+        "A": "Puck"
+      }
+    ]
+  },
+  {
+    "title": "Band Antonyms",
+    "category": "words",
+    "written": "2010-04-07",
+    "tags": "wordplay, band names, music, english, antonyms",
+    "questions": [
+      {
+        "Q": "The Black Dots",
+        "A": "The White Stripes"
+      },
+      {
+        "Q": "Relax. At The Library",
+        "A": "Panic! At The Disco"
+      },
+      {
+        "Q": "Life Bike For Homely",
+        "A": "Death Cab For Cutie"
+      },
+      {
+        "Q": "Casualty",
+        "A": "Suvivor"
+      },
+      {
+        "Q": "Girls From Women",
+        "A": "Boys II Men"
+      },
+      {
+        "Q": "The Whistlecunts",
+        "A": "The Buzzcocks"
+      },
+      {
+        "Q": "Uh Oh Stop",
+        "A": "OK Go"
+      },
+      {
+        "Q": "Revelation",
+        "A": "Genesis"
+      },
+      {
+        "Q": "Expensive Treat",
+        "A": "Cheap Trick"
+      }
+    ]
+  },
+  {
+    "title": "This Round Is Like A Phone That's Attracted To Other Phones",
+    "category": "words",
+    "written": "2010-04-07",
+    "tags": "homophones, wordplay, english",
+    "questions": [
+      {
+        "Q": "An accessory for a horse. Also a kind of party.",
+        "A": "Bridle/Bridal"
+      },
+      {
+        "Q": "An ingredient in baking. Also a thing Van Gogh painted.",
+        "A": "flour/flower"
+      },
+      {
+        "Q": "A small human appendage. Also a thing a big truck can do.",
+        "A": "toe/tow"
+      },
+      {
+        "Q": "A type of aquatic vacation. Also, groups of men working together.",
+        "A": "cruise/crews"
+      },
+      {
+        "Q": "A certain type of nobleman. Also the condition of being infertile.",
+        "A": "baron/barren"
+      },
+      {
+        "Q": "Money's relation to evil. Also a way to get there.",
+        "A": "root/route"
+      },
+      {
+        "Q": "A common illness. Also having been airborne earlier.",
+        "A": "flu/flew"
+      },
+      {
+        "Q": "A shareholder's chief concern. Also one who claims divine inspiration",
+        "A": "profit/prophet"
+      },
+      {
+        "Q": "Of or concerning the mouth. Also of or concerning the ear.",
+        "A": "oral/aural"
+      }
+    ]
+  },
+  {
+    "title": "This Is What Happens In The Fourth Movie",
+    "category": "movies",
+    "written": "2010-04-07",
+    "tags": "movies, sequels",
+    "questions": [
+      {
+        "Q": "Like in all the other films, Warwick Davis is a mythical creature of legend who is keenly interested in gold. However, this one takes place IN SPACE!!!!!",
+        "A": "Leprechaun 4"
+      },
+      {
+        "Q": "In this one, the sweater wearing villain kills the tough biker girl by turning her into a cockroach and then trapping her in a roach motel.",
+        "A": "Nightmare on Elm Street 4"
+      },
+      {
+        "Q": "In this one, the crew travels back to 20th century Earth. There, they meet a punk rocker on the bus and then rescue some blue whales.",
+        "A": "Star Trek IV"
+      },
+      {
+        "Q": "Though the film's mastermind died in the previous movie, he still manages to set up a bunch of retarded torture traps that a police detective wants to solve.",
+        "A": "Saw IV"
+      },
+      {
+        "Q": "The difference this time is that civilians are going to the school in order to learn how to stop crime. Also, this is the last one that Steve Gutenberg stars in.",
+        "A": "Police Academy 4"
+      },
+      {
+        "Q": "Now that he's out of college, Booger is getting married. But the bride's Republican family are prejudiced against people who work in to computers.",
+        "A": "Revenge of the Nerds IV."
+      },
+      {
+        "Q": "The third one had completely different characters, but this one has the return of the guy who isn't Austin Powers, who comes back from the dead to kill more people.",
+        "A": "Halloween 4"
+      },
+      {
+        "Q": "This one's kind of about the Cold War two, because Lex Luthor does bad things with nuclear power.",
+        "A": "Superman 4"
+      },
+      {
+        "Q": "The other ones had the dog playing basketball or football. But in this one, the dog plays baseball! It's funny, no?",
+        "A": "Air Bud 4"
+      },
+    ]
+  },
+  {
+    "title": "Did I Spel That Rite? (The Answer Is 2)",
+    "category": "words",
+    "written": "2010-04-07",
+    "tags": "movies, sequels",
+    "questions": [
+      {
+        "Q": "I find drunkeness to be plesant and even humorous on ocasion.",
+        "A": "3: drunkenness, pleasant, occasion"
+      },
+      {
+        "Q": "Alot of people are probobly indiferent to political matters.",
+        "A": "3: a lot, probably, indifferent"
+      },
+      {
+        "Q": "Athiests ackowlege the argument of God's alleged and apparent absence",
+        "A": "2: a lot, atheists, acknowledge, absence"
+      },
+      {
+        "Q": "Personaly I'd prefer a pleasant prophecy to a plagarized and prejudiced pronounciation.",
+        "A": "3: personally, plagiarized, pronunciation"
+      },
+      {
+        "Q": "The secretery was skillfuly studying speeches on safety, but the sargent surely suprised her.",
+        "A": "4: secretary, skillfully, sergeant, surprised"
+      },
+      {
+        "Q": "I've dicided drunkeness is not disasterous, and disapearing a daiquiri seems doable.",
+        "A": "4: decided, drunkenness, disastrous, disappearing"
+      },
+      {
+        "Q": "I'll congratulate you citizens for capitalizing on this category in this competition.",
+        "A": "0"
+      },
+      {
+        "Q": "I really reccomend that you receive a receipt from this ridiculous restaurant.",
+        "A": "1: recommend"
+      },
+      {
+        "Q": "It's indecient, ignorant and unintelligent to interfere with your child's innoculations because of imaginary issues.",
+        "A": "2: indecent, inoculations"
+      }
+    ]
+  },
+];
